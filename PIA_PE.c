4868fbb424740e5e561cc44e7cbf74fc0b9a201a
IsacLuna <65917254+IsacLuna@users.noreply.github.com>
2020-05-25 14:07:29 -0500

    Update PIA_PE.c

--- PIA_PE.c
+++ PIA_PE.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <conio.h
+
 int main (){
 	printf("Hello world\n");
 	printf("HOLA AMIGOS COMO ESTAN\n");
+	printf("Opino que no hagamos nada y lo dejemos para un d%ca antes de la entrega.\n",161);
+	getch();
 	return 0;
 }
